c48d03b15859df055f0623bcb31f164503d2201b
Prashant Singh Pawar <prashantpawar@gmail.com>
2014-11-15 17:31:55 -0500

    -Added working Mail parsing using node-mbox and mailparser library -Added node-mbox as a dependency

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 (function() {
     var MailParser = require("mailparser").MailParser,
-        mailparser = new MailParser(),
+        Mbox = require('node-mbox'),
+        mbox, mailparser,
         fs = require("fs"),
-        mailFile = "data/mail.mbox"
-    mailparser.on("end", function(mail_object){
-        console.log("From:", mail_object.from); //[{address:'sender@example.com',name:'Sender Name'}]
-        console.log("Subject:", mail_object.subject); // Hello world!
-        console.log("Text body:", mail_object.text); // How are you today?
+        mailFile = "data/mail.mbox";
+        
+    var readStream = fs.createReadStream(mailFile);
+    mbox = new Mbox(readStream);
+    
+    mbox.on('message', function(msg) {
+        mailparser = new MailParser(),
+        console.log("got the message");
+        mailparser.on('end', function(mail_object){
+            console.log("From:", mail_object.from); 
+            console.log("Subject:", mail_object.subject); 
+            // console.log("Text body:", mail_object.text); // How are you today?
+        });
+        mailparser.write(msg);
+        mailparser.end();
     });
-    mailparser._isMbox = true;
-    var readStream= fs.createReadStream(mailFile);
-    readStream.on('open', function () {
-        // This just pipes the read stream to the response object (which goes to the client)
-        readStream.pipe(mailparser);
+    
+    mbox.on('end', function() {
+        console.log('done reading mbox file');
+        // mailparser.end();
     });
+    
 })();
